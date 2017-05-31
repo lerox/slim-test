85fdddeffc37b236c2e224b4a66b63ce624405b3
Leandro Diniz <lerox@hotmail.com>
2017-05-30 22:13:08 -0300

    More endpoints.

--- public/index.php
+++ public/index.php
@@ -5,7 +5,20 @@ require 'vendor/autoload.php';
 $app = new Slim\App();
 
 $app->get('/hello/{name}', function ($request, $response, $args) {
-    return $response->write("Hello, " . $args['name']);
+    return $response->write('Hello, ' . $args['name']);
+});
+
+$app->get('/', function ($request, $response, $args) {
+	$response = $response->withHeader('Content-Type', 'application/json; charset=UTF-8');
+    return $response->withStatus(200)->write('{"message":"It\'s working"}');
+});
+
+$app->get('/ping', function ($req, $res, $args) {
+    return $res->withStatus(200)->write('pong');
+});
+
+$app->get('/bad', function ($req, $res, $args) {
+    return $res->withStatus(400)->write('Bad request my friend!');
 });
 
 $app->run();
